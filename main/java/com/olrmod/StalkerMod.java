0634d44a9a07048fd2f94dc540eda7722f5816a5
gadyushnik <146762265+gadyushnik@users.noreply.github.com>
2025-04-21 19:30:01 +0300

    Update StalkerMod.java

--- main/java/com/olrmod/StalkerMod.java
+++ main/java/com/olrmod/StalkerMod.java
@@ -1,12 +1,18 @@
 package com.olrmod;
 
 import com.olrmod.anomaly.AnomalyLogicHandler;
+import com.olrmod.anomaly.AnomalyConfigLoader;
+import com.olrmod.artifacts.ArtifactData;
 import com.olrmod.artifacts.ArtifactEffectApplier;
-import com.olrmod.artifacts.ArtifactEffectRegistry;
 import com.olrmod.artifacts.ArtifactTooltipHandler;
 import com.olrmod.detectors.DetectorUpdateHandler;
+import com.olrmod.effects.EffectDamageHandler;
 import com.olrmod.emission.EmissionManager;
+import com.olrmod.radiation.RadiationZoneConfig;
+import com.olrmod.radiation.RadiationZoneManager;
+import com.olrmod.safezone.SafeZoneManager;
 import com.olrmod.weight.WeightManager;
+import com.olrmod.armor.ArmorData;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
@@ -20,16 +26,21 @@ public class StalkerMod {
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        // Регистрируем обработчики
+        // === Регистрация обработчиков ===
         MinecraftForge.EVENT_BUS.register(new EmissionManager());
         MinecraftForge.EVENT_BUS.register(new AnomalyLogicHandler());
         MinecraftForge.EVENT_BUS.register(new EffectDamageHandler());
         MinecraftForge.EVENT_BUS.register(new DetectorUpdateHandler());
         MinecraftForge.EVENT_BUS.register(new ArtifactEffectApplier());
         MinecraftForge.EVENT_BUS.register(new ArtifactTooltipHandler());
+        MinecraftForge.EVENT_BUS.register(new RadiationZoneManager());
+        MinecraftForge.EVENT_BUS.register(new SafeZoneManager());
 
-        // Загружаем JSON-конфиги
-        ArtifactEffectRegistry.loadArtifacts();
+        // === Загрузка конфигов ===
+        ArtifactData.load();
         WeightManager.loadWeights(Loader.instance().getConfigDir());
+        AnomalyConfigLoader.load();
+        RadiationZoneConfig.load();
+        ArmorData.load(); // Загрузка конфигурации брони
     }
 }
